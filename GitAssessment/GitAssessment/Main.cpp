a06564dc8cb65a3b8dbabb19db612f1b6ee48a8f
Lachlan Brown <ldbrown7649@gmail.com>
2022-05-17 11:04:50 +1000

    Add text to "GitAssessment" VS project.

--- GitAssessment/GitAssessment/Main.cpp
+++ GitAssessment/GitAssessment/Main.cpp
@@ -2,4 +2,8 @@
 
 int main() {
 	std::cout << "Hello, Git!" << std::endl;
+	std::cout << "This is a file for practicing how to use Git." << std::endl;
+	std::cout << "It will be pushed to the feat-onboarding branch before being merged into the develop branch" << std::endl;
+
+	return 0;
 }
