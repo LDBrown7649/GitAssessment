90441a2c50f7ca811827ebdd37a13b366a9dd44f
Lachlan Brown <ldbrown7649@gmail.com>
2022-05-17 16:33:32 +1000

    Add comments and pause before closing.

--- GitAssessment/GitAssessment/Main.cpp
+++ GitAssessment/GitAssessment/Main.cpp
@@ -1,20 +1,29 @@
 #include <iostream>
 
 int main() {
+
+	// Display messages explaining what this file is for.
 	std::cout << "Hello, Git!" << std::endl;
 	std::cout << "This is a file for practicing how to use Git." << std::endl;
-	std::cout << "It will be pushed to the feat-onboarding branch before being merged into the develop branch" << std::endl;
 
 	std::cout << std::endl;
-	// Prints the letters of the alphabet.
-	std::cout << "Printing all the letters of the alphabet (Lower):" << std::endl;
+	// Prints all of the letters in lowercase.
+	std::cout << "Printing all the letters of the alphabet (Lowercase):" << std::endl;
 	for (int i = 0; i < 26; i++) {
-		std::cout << (char)(97 + i) << std::endl;
+		std::cout << (char)(97 + i);
 	}
-	std::cout << "Prints all letters in the alphabet (Upper):" << std::endl;
+	std::cout << std::endl;
+
+	// Prints all of the letters in uppercase.
+	std::cout << std::endl;
+	std::cout << "Prints all letters in the alphabet (Uppercase):" << std::endl;
 	for (int i = 0; i < 26; i++) {
-		std::cout << (char)(65 + i) << std::endl;
+		std::cout << (char)(65 + i);
 	}
+	std::cout << std::endl;
+
+	std::cout << "Press enter to quit" << std::endl;
+	std::cin.get();
 
 	return 0;
 }
