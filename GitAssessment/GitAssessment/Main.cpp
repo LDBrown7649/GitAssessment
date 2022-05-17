ef0c0af479ebda17a2cb521f7c8b8cf90b744570
Lachlan Brown <ldbrown7649@gmail.com>
2022-05-17 11:33:17 +1000

    Squashed commit of the following:
    commit 04a95fe44a00083f45935d31188759ee7371d9e3
    Author: Lachlan Brown <ldbrown7649@gmail.com>
    Date:   Tue May 17 11:25:31 2022 +1000
    
        Add Alphabet Print

--- GitAssessment/GitAssessment/Main.cpp
+++ GitAssessment/GitAssessment/Main.cpp
@@ -5,5 +5,12 @@ int main() {
 	std::cout << "This is a file for practicing how to use Git." << std::endl;
 	std::cout << "It will be pushed to the feat-onboarding branch before being merged into the develop branch" << std::endl;
 
+	std::cout << std::endl;
+	// Prints the letters of the alphabet.
+	for (int i = 0; i < 26; i++) {
+		std::cout << (char)(65 + i);
+	}
+	std::cout << std::endl;
+
 	return 0;
 }
